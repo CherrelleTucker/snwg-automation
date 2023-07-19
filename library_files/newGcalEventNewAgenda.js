5b303ff3186ca10fe13f2cc6efca857afe81ab9b
Cherrelle <cherrelle.j.tucker@nasa.gov>
2023-07-18 21:01:21 -0500

    restructure, function for custom agenda exclusion

--- library_files/newGcalEventNewAgenda.js
+++ library_files/newGcalEventNewAgenda.js
@@ -1,52 +1,96 @@
 // Purpose: general agenda is created and stored in a top-level folder for the day's Google Calendar (Conference Room Reservation) events
 // Calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com
 // Template ID: 1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE
-// Folder id for newly created document to be placed in: 16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr. 
+// Folder id for newly created document to be placed in: 16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr 
 
-// Done: Make a copy of the template document
-// Done: Set the new document's name with the date of the calendar event + title of the event; format YYYY-MM-DD
-// Done: Replace Placeholder text: {{Meeting Name}} with the same name as the meeting on the calendar; 
-// Done: Replace placeholder text: {{Date of Calendar Event}} with the same date as the calendar event; Format the date as "Day, Month Date, Year"  
-// Done: Replace placeholder text: {{Time of Calendar Event}} with the same time range as the calendar event; Format the time as "beginning HH:MM - ending HH:MM Timezone"
-// Done: Replace placeholder text {{Conference Room}} with the Location of the created meeting.
-// Done: Replace the place holder text {{Meeting Owner Name}} with the email address of the person who created the meeting. 
-//      Note that {{Meeting Owner Name}} is replaced with the email address of the event creator, as Google Apps Script does not have a direct method to get the creator's name.
-// Done: triggers creation of documents for events that have a specific owner email address. The first time you run createTimeDrivenTrigger function, it will create a time-driven trigger to execute the createDocFromFutureEvents function every 5 minutes. 
-// Done: Added custom menu: 
-// Future Development: Exlude routine meetings? ie: Internal and Monthly have custom agendas and scripts. 
-// Future Development: Eventually would like thtis to automatically sort to the appropriate project folder. 
+// event is created in IMPACT conference room calendar - calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com
+// trigger: script runs once per day 
+// Search calendar for events for day after current day and later.
+// Check "Created by" id (owner email address) against specified email address.
+// If no match, abort action
+// If match, create document from specified template and place in specified folder.
+// Find date of calendar event. Format in YYYY-MM-DD; name document "<date of the calendar event> + title of the event"
+// Find name of calendar event. Replace {{Meeting Name}} placeholder text in new document.
+// Find date of calendar event. Replace {{Date of Calendar Event}} placeholder text in new document. Format the date as "Day, Month Date, Year"
+// Find time of calendar event. Replace {{Time of Calendar Event}} placeholder text in new document. Format the time as "beginning HH:MM - ending HH:MM Timezone". Timezone = CT
+// Find location of calendar event. Replace {{Conference Room}} placeholder text in new document.
+// Find "created by" email address. Replace {{Meeting Owner Name}} placeholder text in new document.
+//      Note that {{Meeting Owner Name}} is replaced with the email address of the event creator and not the owner name, as Google Apps Script does not have a direct method to get the creator's name.
 
-function onOpen(){
-  DocumentApp.getUi()
-  .createMenu('Action Items')
-  .addItem('Get Actions','createDocFromFutureEvents')
-  .addToUi();
-}
+// Future Development: Eventually would like this to automatically sort to the appropriate project folder. 
 
-function createDocFromFutureEvents() {
-  var calendarId = 'mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com'; //Test calendar; replace with mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com when ready to deploy
-  var templateId = '1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE';
-  var targetFolderId = '16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr';
-  
-  // Add the specific owner's email address here
-  var specificOwnerEmail = 'cherrelle.j.tucker@nasa.gov';
-
-  var calendar = CalendarApp.getCalendarById(calendarId);
+// Helper function: Get future events from a Google Calendar
+function getFutureEvents(calendarId) {
   var today = new Date();
   var aMonthFromNow = new Date();
   aMonthFromNow.setDate(today.getDate() + 30);
-
+  
+  var calendar = CalendarApp.getCalendarById(calendarId);
   var events = calendar.getEvents(today, aMonthFromNow);
+
+  return events;
+}
+
+// Helper function: Create a new document by making a copy of a template document
+function createDocumentFromTemplate(templateId, folder, documentName) {
+  var template = DriveApp.getFileById(templateId);
+  var newFile = template.makeCopy(documentName, folder);
+
+  return DocumentApp.openById(newFile.getId());
+}
+
+// Helper function: Replace placeholders in the document with event details
+function replacePlaceholdersInDoc(doc, event) {
+  var eventTitle = event.getTitle();
+  var eventDate = event.getStartTime();
+  var eventEndDate = event.getEndTime();
+  
+  var body = doc.getBody();
+  body.replaceText('{{Meeting Name}}', eventTitle);
+  
+  var longFormattedDate = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'EEEE, MMMM dd, yyyy');
+  body.replaceText('{{Date of Calendar Event}}', longFormattedDate);
+  
+  var eventTime = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'HH:mm') + 
+                    ' - ' + Utilities.formatDate(eventEndDate, Session.getScriptTimeZone(), 'HH:mm z');
+  body.replaceText('{{Time of Calendar Event}}', eventTime);
+  
+  var eventLocation = event.getLocation() || 'No location specified'; //Get the location or set a default message
+  body.replaceText('{{Conference Room}}', eventLocation);
+  
+  body.replaceText('{{Meeting Owner Name}}', event.getCreators()[0]); //Replace with the creator's email address
+
+  doc.saveAndClose();
+}
+
+// Helper function: Check if an event has a custom agenda template or otherwise should not be made; remove this function if not if not exculding routine events with custom agendas
+function isEventRecurring(event) {
+  return event.getDescription().toLowerCase().includes('custom agenda');
+}
+
+
+// Primary function: Find future events and create a general agenda document from them
+function createDocFromFutureEvents() {
+  var calendarId = 'mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com'; 
+  var templateId = '1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE';
+  var targetFolderId = '16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr';
+
+  // Add the specific owner's email address here
+  var specificOwnerEmail = 'cherrelle.j.tucker@nasa.gov';
+
+  var events = getFutureEvents(calendarId);
   
   var scriptProperties = PropertiesService.getScriptProperties();
   var processedEvents = scriptProperties.getProperty('processedEvents');
   processedEvents = processedEvents ? JSON.parse(processedEvents) : {};
 
+  var targetFolder = DriveApp.getFolderById(targetFolderId);
+
   for (var i = 0; i < events.length; i++) {
     var event = events[i];
     var eventId = event.getId();
 
-    if (processedEvents[eventId]) {
+    if (processedEvents[eventId] || isEventRecurring(event)) { // <<--Remove "|| isEventRecurring(event)" if not exculding routine events with custom agendas
       continue;
     }
 
@@ -59,108 +103,14 @@ function createDocFromFutureEvents() {
 
     var eventTitle = event.getTitle();
     var eventDate = event.getStartTime();
-    var eventEndDate = event.getEndTime();
-
     var formattedDate = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'yyyy-MM-dd');
     var docTitle = formattedDate + ' ' + eventTitle;
 
-    var targetFolder = DriveApp.getFolderById(targetFolderId);
-    var file = DriveApp.getFileById(templateId).makeCopy(docTitle, targetFolder);
-
-    var doc = DocumentApp.openById(file.getId());
-    var body = doc.getBody();
-
-    body.replaceText('{{Meeting Name}}', eventTitle);
-
-    var longFormattedDate = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'EEEE, MMMM dd, yyyy');
-    body.replaceText('{{Date of Calendar Event}}', longFormattedDate);
-
-    var eventTime = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'HH:mm') + 
-                    ' - ' + Utilities.formatDate(eventEndDate, Session.getScriptTimeZone(), 'HH:mm z');
-    body.replaceText('{{Time of Calendar Event}}', eventTime);
-
-    var eventLocation = event.getLocation() || 'No location specified'; //Get the location or set a default message
-    body.replaceText('{{Conference Room}}', eventLocation);
-
-    body.replaceText('{{Meeting Owner Name}}', eventCreatorEmail); //Replace with the creator's email address
-
-    doc.saveAndClose();
+    var doc = createDocumentFromTemplate(templateId, targetFolder, docTitle);
+    replacePlaceholdersInDoc(doc, event);
     
     processedEvents[eventId] = true;
   }
 
   scriptProperties.setProperty('processedEvents', JSON.stringify(processedEvents));
 }
-
-  
-
-function createDocFromFutureEvents() {
-  var calendarId = 'mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com'; //Test calendar; replace with mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com when ready to deploy
-  var templateId = '1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE';
-  var targetFolderId = '16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr';
-  
-  // Add the specific owner's email address here
-  var specificOwnerEmail = 'cherrelle.j.tucker@nasa.gov';
-
-  var calendar = CalendarApp.getCalendarById(calendarId);
-  var today = new Date();
-  var aMonthFromNow = new Date();
-  aMonthFromNow.setDate(today.getDate() + 30);
-
-  var events = calendar.getEvents(today, aMonthFromNow);
-  
-  var scriptProperties = PropertiesService.getScriptProperties();
-  var processedEvents = scriptProperties.getProperty('processedEvents');
-  processedEvents = processedEvents ? JSON.parse(processedEvents) : {};
-
-  for (var i = 0; i < events.length; i++) {
-    var event = events[i];
-    var eventId = event.getId();
-
-    if (processedEvents[eventId]) {
-      continue;
-    }
-
-    var eventCreatorEmail = event.getCreators()[0]; //Get the creator's email address
-    
-    // Check if the event's creator is the specific owner
-    if (eventCreatorEmail !== specificOwnerEmail) {
-      continue;
-    }
-
-    var eventTitle = event.getTitle();
-    var eventDate = event.getStartTime();
-    var eventEndDate = event.getEndTime();
-
-    var formattedDate = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'yyyy-MM-dd');
-    var docTitle = formattedDate + ' ' + eventTitle;
-
-    var targetFolder = DriveApp.getFolderById(targetFolderId);
-    var file = DriveApp.getFileById(templateId).makeCopy(docTitle, targetFolder);
-
-    var doc = DocumentApp.openById(file.getId());
-    var body = doc.getBody();
-
-    body.replaceText('{{Meeting Name}}', eventTitle);
-
-    var longFormattedDate = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'EEEE, MMMM dd, yyyy');
-    body.replaceText('{{Date of Calendar Event}}', longFormattedDate);
-
-    var eventTime = Utilities.formatDate(eventDate, Session.getScriptTimeZone(), 'HH:mm') + 
-                    ' - ' + Utilities.formatDate(eventEndDate, Session.getScriptTimeZone(), 'HH:mm z');
-    body.replaceText('{{Time of Calendar Event}}', eventTime);
-
-    var eventLocation = event.getLocation() || 'No location specified'; //Get the location or set a default message
-    body.replaceText('{{Conference Room}}', eventLocation);
-
-    body.replaceText('{{Meeting Owner Name}}', eventCreatorEmail); //Replace with the creator's email address
-
-    doc.saveAndClose();
-    
-    processedEvents[eventId] = true;
-  }
-
-  scriptProperties.setProperty('processedEvents', JSON.stringify(processedEvents));
-}
-
-  
