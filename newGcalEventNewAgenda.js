3261cf3adc93073f11b07c528dd39b10c5535419
CherrelleTucker <106271365+CherrelleTucker@users.noreply.github.com>
2023-08-01 13:50:02 -0500

    Use instructions Update newGcalEventNewAgenda.js

--- newGcalEventNewAgenda.js
+++ newGcalEventNewAgenda.js
@@ -1,29 +1,32 @@
 // Purpose: general agenda is created and stored in a top-level folder for the day's Google Calendar (Conference Room Reservation) events
-// How to use: create a Google Docs template document with {{Meeting Name}} {{Date of Calendar Event}}	{{Time of Calendar Event}} {{Conference Room}} {{Conference Room}} placeholders
-// in the script, replace General meeting agenda Template ID with chosen template ID
-// in the script, replace Folder id for newly created document to be placed in with the chosen folder ID
-// in the script, replace 'custom agenda' with your chosen exclusion phrase to keep new agenda from being made 
-// in the script, select "Triggers" tab (alarm clock) and add a trigger
-// in the IMPACT conference room calendar, create a future event with the title of the meeting and location. 
-// either run function createDocFromFutureEvents or wait for your trigger to run. 
+// How to use: 
+// In your Google Workspace: create a Google Docs template document.
+// In the template document: create fields {{Meeting Name}} {{Date of Calendar Event}}	{{Time of Calendar Event}} {{Conference Room}} {{Conference Room}} placeholders
+// In Google Apps Script (https://script.google.com/home): create a new script with the following information.
+// in the script: replace General meeting agenda Template ID with chosen template ID
+// in the script: replace Folder id for newly created document to be placed in with the chosen folder ID
+// in the script: replace 'custom agenda' with your chosen exclusion phrase to keep new agenda from being made 
+// in the script: select "Triggers" tab (alarm clock) and add a trigger
+// in the IMPACT conference room calendar: create a future event with the title of the meeting and location. 
+// in the script: either run function createDocFromFutureEvents or wait for your trigger to run. 
 
 // IMPACT Conference Room Calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com
-// General meeting agenda Template ID: 1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE <--replace with chosen template
-// Folder id for newly created document to be placed in: 16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr <--replace with chosen folder
+// General meeting agenda Template ID: 1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE <--replace with your chosen template
+// Folder id for newly created document to be placed in: 16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr <--replace with your chosen folder
 
-// event is created in IMPACT conference room calendar - calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com
-// trigger: set in script settings 
+// event is created in IMPACT conference room calendar - calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com 
 // Search calendar for events for the day after current day and later.
 // Check "Created by" id (owner email address) against specified email address.
-// If no match, abort action
-// If match, create document from specified template and place in specified folder.
+  // If no match, abort action
+  // If match, create document from specified template and place in specified folder.
 // Find date of calendar event. Format in YYYY-MM-DD; name document "<date of the calendar event> + title of the event"
 // Find name of calendar event. Replace {{Meeting Name}} placeholder text in new document.
 // Find date of calendar event. Replace {{Date of Calendar Event}} placeholder text in new document. Format the date as "Day, Month Date, Year"
 // Find time of calendar event. Replace {{Time of Calendar Event}} placeholder text in new document. Format the time as "beginning HH:MM - ending HH:MM Timezone". Timezone = CT
 // Find location of calendar event. Replace {{Conference Room}} placeholder text in new document.
 // Find "created by" email address. Replace {{Meeting Owner Name}} placeholder text in new document.
-//      Note that {{Meeting Owner Name}} is replaced with the email address of the event creator and not the owner name, as Google Apps Script does not have a direct method to get the creator's name.
+  // Note that {{Meeting Owner Name}} is replaced with the email address of the event creator and not the owner name, as Google Apps Script does not have a direct method to get the creator's name.
+// trigger: set in script settings
 
 /////////////////////////////////////////////
 
