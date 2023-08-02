64330404aa7cf11deca634612efa8cd53e8533b5
Cherrelle <cherrelle.j.tucker@nasa.gov>
2023-08-02 18:26:20 -0500

    updated global variables and use instructions

--- newGcalEventNewAgenda.js
+++ newGcalEventNewAgenda.js
@@ -1,34 +1,32 @@
-// Purpose: general agenda is created and stored in a top-level folder for the day's Google Calendar (Conference Room Reservation) events
-// How to use: 
-// In your Google Workspace: create a Google Docs template document.
-// In the template document: create fields {{Meeting Name}} {{Date of Calendar Event}}	{{Time of Calendar Event}} {{Conference Room}} {{Conference Room}} placeholders
-// In Google Apps Script (https://script.google.com/home): create a new script with the following information.
-// in the script: replace General meeting agenda Template ID with chosen template ID
-// in the script: replace Folder id for newly created document to be placed in with the chosen folder ID
-// in the script: replace 'custom agenda' with your chosen exclusion phrase to keep new agenda from being made 
-// in the script: select "Triggers" tab (alarm clock) and add a trigger
-// in the IMPACT conference room calendar: create a future event with the title of the meeting and location. 
-// in the script: either run function createDocFromFutureEvents or wait for your trigger to run. 
+// Purpose: 
+// automatically create new agenda documents for specific events in a Google Calendar. It scans the calendar for future events within the next 30 days and checks if the event's creator matches a specific owner's email address. For eligible events, it generates a new agenda document by making a copy of a provided template, populating it with event details like meeting name, date, time, and location. The script also excludes events with a "custom agenda" description and tracks processed events to avoid duplicating agendas for the same event.
 
-// IMPACT Conference Room Calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com
-// General meeting agenda Template ID: 1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE <--replace with your chosen template
-// Folder id for newly created document to be placed in: 16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr <--replace with your chosen folder
+// Future Development: Automatically place agenda in the appropriate project folder. 
 
-// event is created in IMPACT conference room calendar - calendar ID: mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com 
-// Search calendar for events for the day after current day and later.
-// Check "Created by" id (owner email address) against specified email address.
-  // If no match, abort action
-  // If match, create document from specified template and place in specified folder.
-// Find date of calendar event. Format in YYYY-MM-DD; name document "<date of the calendar event> + title of the event"
-// Find name of calendar event. Replace {{Meeting Name}} placeholder text in new document.
-// Find date of calendar event. Replace {{Date of Calendar Event}} placeholder text in new document. Format the date as "Day, Month Date, Year"
-// Find time of calendar event. Replace {{Time of Calendar Event}} placeholder text in new document. Format the time as "beginning HH:MM - ending HH:MM Timezone". Timezone = CT
-// Find location of calendar event. Replace {{Conference Room}} placeholder text in new document.
-// Find "created by" email address. Replace {{Meeting Owner Name}} placeholder text in new document.
-  // Note that {{Meeting Owner Name}} is replaced with the email address of the event creator and not the owner name, as Google Apps Script does not have a direct method to get the creator's name.
-// trigger: set in script settings
+// To Note: 
+// This script is developed as a Google Apps Script standalone script. It is designed to operate independently and does not require any external application or service to function. It is a self-contained piece of code with a time-based daily trigger.
 
-/////////////////////////////////////////////
+// To Use: 
+// 1. Prepare Google Calendar and Template: Ensure you have the required access and permissions to create events in Google Calendar. Also, prepare a template for the agenda in Google Docs and note down its Template ID.
+// 2. Open the Script Editor: Access the Google Apps Script editor by clicking on "Extensions" in the top menu and choosing "Apps Script.
+// 3. Copy and Paste the Script: Copy the entire script provided above and paste it into the Google Apps Script editor
+// 4. Configure Global Variables: Adjust the global variables at the beginning of the script to match your Template ID, the calendar ID for the "IMPACT conference room calendar," and the folder ID where you want to store the new agenda documents
+// 5. Save the Script: Save the script by clicking on the floppy disk icon or pressing "Ctrl + S" (Windows) or "Cmd + S" (Mac)
+// 6. Run the Script: To generate new agenda documents, click on the play button ▶️ in the Google Apps Script editor or manually run the `createDocFromFutureEvents()` function.
+// 7. Agenda Generation: The script will find future events within the next 30 days in the specified calendar and create new agenda documents based on the provided template. Each document will be named with the event date and title.
+// 8. Customize Agenda Exclusion (Optional): If you want to exclude certain events from having agendas generated, modify the `isEventRecurring()` function as indicated in the comments.
+// 9. Hyperlink to Previous Agenda: The script will automatically search for the most recent previous month's agenda in the folder (excluding the newly created one) and replace the placeholder "{{link to last SNWG/NASA monthly}}" with a hyperlink to that agenda.
+// 10. Schedule Automation (Optional): If you want to generate agendas automatically, set up a trigger to run the script at a specific interval (e.g., daily). To do this, click on the clock icon in the Google Apps Script editor, add a new trigger, and set it to run the `createDocFromFutureEvents()` function on the desired schedule.
+// 11. Ensure Permission: When running the script for the first time, it may ask for permission to access your Google Calendar and Drive. Click "Continue" and grant the necessary permissions.
+// Before executing the script, carefully review its functionality, and customize it according to your requirements. Also, ensure that the script has access to the appropriate folders and files in your Google Drive and Calendar. 
+
+///////////////////////////////////////////////
+
+// Global variables
+var calendarId = 'mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com'; // IMPACT conference room calendar
+var templateId = '1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE'; // SNWG MO Template: YYYY-MM-DD Meeting Agenda
+var targetFolderId = '16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr'; // Meeting Notes> Auto generated New Agendas SNWG MO Google drive folder
+var specificOwnerEmail = 'cherrelle.j.tucker@nasa.gov'; // specific owner's email address
 
 // Helper function: Get future events from a Google Calendar
 function getFutureEvents(calendarId) {
@@ -69,42 +67,34 @@ function replacePlaceholdersInDoc(doc, event) {
   var eventLocation = event.getLocation() || 'No location specified'; //Get the location or set a default message
   body.replaceText('{{Conference Room}}', eventLocation);
   
-  body.replaceText('{{Meeting Owner Name}}', event.getCreators()[0]); //Replace with the creator's email address
+  body.replaceText('{{Meeting Owner Name}}', event.getCreators()[0]); // {{Meeting Owner Name}} is replaced with the email address of the event creator and not the owner name, as Google Apps Script does not have a direct method to get the creator's name.
 
   doc.saveAndClose();
 }
 
 // Helper function: Check if an event has a custom agenda template or otherwise should not be made; remove this function if not if not exculding routine events with custom agendas
 function isEventRecurring(event) {
-  return event.getDescription().toLowerCase().includes('custom agenda'); // <--replace with your chosen exclusion phrase to keep new agenda from being made 
+  return event.getDescription().toLowerCase().includes('custom agenda');
 }
 
 // Primary function: Find future events and create a general agenda document from them
 function createDocFromFutureEvents() {
-  var calendarId = 'mn9msmqj2nqobs0md4gmgfnabk@group.calendar.google.com'; 
-  var templateId = '1nZN2y09ad2V7CqK0QIqrFXB_lZvv4XyXtefbOzdpdpE'; // <-- replace with desired template
-  var targetFolderId = '16oXc4f1Rlvwk0vsXa27Dvy-niH1E1IOr'; // <-- replace with desired target folder
-
-  // Add the specific owner's email address here
-  var specificOwnerEmail = 'cherrelle.j.tucker@nasa.gov'; // <-- replace with desired meeting creator's email
-
   var events = getFutureEvents(calendarId);
-  
   var scriptProperties = PropertiesService.getScriptProperties();
   var processedEvents = scriptProperties.getProperty('processedEvents');
-  processedEvents = processedEvents ? JSON.parse(processedEvents) : {};
 
+  processedEvents = processedEvents ? JSON.parse(processedEvents) : {};
   var targetFolder = DriveApp.getFolderById(targetFolderId);
 
   for (var i = 0; i < events.length; i++) {
     var event = events[i];
     var eventId = event.getId();
 
-    if (processedEvents[eventId] || isEventRecurring(event)) { // <<-- Remove "|| isEventRecurring(event)" if not exculding routine events with custom agendas
+    if (processedEvents[eventId] || isEventRecurring(event)) { // <<--Remove "|| isEventRecurring(event)" if not exculding routine events with custom agendas
       continue;
     }
 
-    var eventCreatorEmail = event.getCreators()[0]; // Get the creator's email address
+    var eventCreatorEmail = event.getCreators()[0]; //Get the creator's email address
     
     // Check if the event's creator is the specific owner
     if (eventCreatorEmail !== specificOwnerEmail) {
