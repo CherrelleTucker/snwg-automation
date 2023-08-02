e26bad32664ea529cf07412e643856e7194ca7a8
Cherrelle <cherrelle.j.tucker@nasa.gov>
2023-08-02 12:55:37 -0500

    corrected entire script, +test funct. +descrip

--- createNewSMDLLMagendaIMPACT.js
+++ createNewSMDLLMagendaIMPACT.js
@@ -1,213 +1,97 @@
-// Purpose: 
-// 1. Search agendas for action items to be completed and populate in the Action Tracking Google Sheet.
-// Future development: preserve links in task items when copying to action table
+// Purpose:  
+// Create a bi-weekly agenda for the "SMD Large Language Model WG" (Working Group) by utilizing a template agenda document and populating it with the appropriate meeting date. The script will only create the agenda if the current week number is even (bi-weekly) to align with the group's bi-weekly meeting schedule.
 
-// This script is developed for use as either as a Google Apps Script container script or as a Google Apps Script library script. 
-// Google Apps Script container script: a script that is bound to a specific file, such as a Google Sheets, Google Docs, or Google Forms file. This container script acts as the file's custom script, allowing users to extend the functionality of the file by adding custom functions, triggers, and menu items to enhance its behavior and automation.
+// Future development: 
+// none currently identified
 
-// Google Apps Script library script: a self-contained script that contains reusable functions and can be attached to multiple projects or files. By attaching the library script to different projects, developers can access and use its functions across various files, enabling code sharing and improving code maintenance and version control.
+// To note: 
+// This script is developed as a Google Apps Script standalone script. It is developed to operate independently and does not require any external application or service to function. It is a self-contained piece of code with a time-based daily trigger.
 
-//////////////////////////////////////////////////
+// To use: 
+// 1. Modify the global variables AGENDA_TEMPLATE_ID and AGENDA_FOLDER_ID to the respective IDs of your agenda template document and destination folder where the agendas should be stored. These variables should be set with the appropriate values specific to your Google Drive.
+// 2. The primary function createBiweeklyAgenda() will be triggered automatically if set up with a time-based daily trigger. It will check if the current week number is even, and if so, it will create a new agenda document for the "SMD Large Language Model WG."
+// 3. To test the agenda creation manually, use the test function testEvenWeekAgendaCreation(). This function simulates an even week by setting the weekNumber variable to an even value (e.g., 32). When calling this test function, it will create an agenda document for the simulated even week.
+// 4. When deploying the script as a standalone app, make sure to set up a time-based daily trigger for the primary function createBiweeklyAgenda(). This trigger will automatically create the agenda at the appropriate time, provided that it's an even week.
+// 5. The script will create a new agenda document, customize its title with the appropriate meeting date (the following Wednesday), and replace the placeholder text "{{Day, Month Date, YYYY}}" within the document's body with the calculated meeting date in "Day, Month Date, YYYY" format.
+// 6. The agenda creation process is automated and efficient, saving time and effort for the "SMD Large Language Model WG" by eliminating manual document generation and date calculation.
 
-// To use this script as a library script, follow the instructions below:
-// 1. Obtain the script ID of the inDocActionItems library script.
-//  script ID: 1kBbrOJCXewvSixfq1yR8d-lEtgDG5yzD9-pqPeuC9ugLka7gQULwkBH_ <-- verify current library script id by checking in Project settings (gear icon).
-// 2. Open the container document where you want to use this script.
-// 3. Click on the "Project settings" gear icon in the script editor.
-// 4. In the "Script ID" field, replace the existing script ID with the script ID of the inDocActionItems library script.
-// 5. Click "Save" to update the script ID.
+//////////////////////////////////////////////////////
 
-// function to use in your container to call inDocActionItems as a library script: 
-// function libraryCall(){
-//  inDocActionItems.runBothActionItems();
-//  inDocActionItems.customMenu();
-//}
+// Global constant for Agenda Template ID and Agenda Destination Folder ID
+var AGENDA_TEMPLATE_ID = "1NbArx4AptMdFYTeLHaFx2LiKCKYpoSsOrreHY8EaamY"; // Template Agenda for SMD {{short date}}
+var AGENDA_FOLDER_ID = "1ReHobNb95r5oLKXabg2JC5Zgg0ZmJLfH"; // SMD Large Language Model WG Google Drive folder
 
-///////////////////////////////////////////////////
+// Helper Function: Get the next meeting date (Wednesday)
+function getNextWednesday() {
+  var currentDate = new Date();
+  var daysUntilNextWednesday = (3 - currentDate.getDay() + 7) % 7;
+  return new Date(currentDate.getTime() + (daysUntilNextWednesday * 24 * 60 * 60 * 1000));
+}
 
-// function to create custom menu with buttons on document open
-function onOpen() {
-    DocumentApp.getUi() // 
-        .createMenu('Custom Menu')
-        .addItem('Populate Actions','actionsFromParagraphs')      
-        .addToUi();
-  }
-  
-  // Primary function that extracts action items from paragraphs in the document and populates them into an action item table.
-  function actionsFromParagraphs() {
-    const document = DocumentApp.getActiveDocument();
-    const body = document.getBody();
-    const actionsPhrases = ['Action:', 'Action : ', 'Action-', 'Action -','action:', 'action : ', 'action-', 'action -'];
-    const actionList = [];
-  
-    const paragraphs = body.getParagraphs();
-    let isActionParagraph = false;
-    paragraphs.forEach(paragraph => {
-      const text = paragraph.getText();
-      actionsPhrases.forEach(actionsPhrase => {
-        const startIndex = text.indexOf(actionsPhrase);
-  
-        if (startIndex !== -1) {
-          isActionParagraph = true;
-          const sentence = text.substring(startIndex + actionsPhrase.length);
-          const words = sentence.split(" ");
-  
-          actionList.push({ action: '□ ' + words.join(' ') });
-        }
-      });
-    });
-  
-    if (isActionParagraph) {
-      let existingTable = findExistingActionTable(body);
-      if (existingTable) {
-        const existingActions = getExistingActions(existingTable);
-        for (const actionItem of actionList) {
-          const isDuplicate = checkIfActionExistsInTable(existingActions, actionItem);
-          if (!isDuplicate) {
-            populateActionInTable(existingTable, actionItem);
-          }
-        }
-        Logger.log('Actions populated from paragraphs.');
-      } else {
-        existingTable = createSecondTable(body);
-        for (const actionItem of actionList) {
-          populateActionInTable(existingTable, actionItem);
-        }
-        Logger.log('Actions populated from paragraphs. New action item table created.');
-      }
-    } else {
-      Logger.log('No action items found in paragraphs.');
-    }
-  }
-  
-  
-  // Helper function that creates a new action item table in the document.
-  function createSecondTable(body) {
-    const table = body.appendTable();
-    return table;
-  }
-  
-  // Helper function that retrieves existing action items from the table in the document.
-  function getExistingActions(table) {
-    const existingActions = [];
-    const numRows = table.getNumRows();
-  
-    for (let i = 0; i < numRows; i++) { // Start from 0 as there is no header row now
-      const row = table.getRow(i);
-      const actionContent = row.getCell(0).getText();
-      existingActions.push({ action: actionContent });
-    }
-  
-    return existingActions;
-  }
-  
-  // Helper function that checks if an action item already exists in the table.
-  function checkIfActionExistsInTable(existingActions, actionItem) {
-    for (const existingAction of existingActions) {
-      if (existingAction.action === actionItem.action) {
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  // Helper function that populates an action item into the table.
-  function populateActionInTable(table, actionItem) {
-    const numRows = table.getNumRows();
-  
-    for (let i = 0; i < numRows; i++) { // Start from 0 as there is no header row now
-      const row = table.getRow(i);
-      const cell = row.getCell(0);
-  
-      if (cell.getText() === '') {
-        cell.setText(actionItem.action);
-        return;
-      }
-    }
-  
-    const newRow = table.appendTableRow();
-    newRow.appendTableCell().setText(actionItem.action); // Action cell
-    table.setBorderColor('#FFFFFF'); // Set border color to be transparent
-  }
-  
-  // Primary function that extracts action items from an existing table in the document and populates them into the Action Items table.
-  function actionsFromTable() {
-    const document = DocumentApp.getActiveDocument();
-    const body = document.getBody();
-    let existingTable = findExistingActionTable(body);
-  
-    if (existingTable) {
-      const actionList = extractActionsFromTable(existingTable);
-  
-      if (actionList.length > 0) {
-        const existingActions = getExistingActions(existingTable);
-  
-        for (const actionItem of actionList) {
-          const isDuplicate = checkIfActionExistsInTable(existingActions, actionItem);
-  
-          if (!isDuplicate) {
-            populateActionInTable(existingTable, actionItem);
-          }
-        }
-  
-        Logger.log('Actions populated from table.');
-      } else {
-        Logger.log('No action items found in the existing table.');
-      }
-    } else {
-      Logger.log('No existing action item table found.');
-    }
-  }
-  
-  // Helper function that searches for an existing action item table in the document.
-  function findExistingActionTable(body) {
-    const tables = body.getTables();
-    for (let i = 0; i < tables.length; i++) {
-      const table = tables[i];
-      const numRows = table.getNumRows();
-  
-      if (numRows > 0) {
-        const firstRowFirstCellText = table.getRow(0).getCell(0).getText();
-  
-        if (firstRowFirstCellText.startsWith('□')) {
-          return table;
-        }
-      }
-    }
-  
-    return null;
-  }
-  
-  // Helper function: extractActionsFromTable
-  function extractActionsFromTable(table) {
-    const actionList = [];
+// Helper Function: Format the meeting date as "M.d.yy"
+function formatMeetingDate(date) {
+  return Utilities.formatDate(date, Session.getScriptTimeZone(), "M.d.yy");
+}
+
+// Helper Function: Replace a placeholder with a formatted date
+function replaceWithFormattedDate(documentBody, placeholderText, currentDate) {
+  var dateForInternal = new Date(currentDate);
+  dateForInternal.setDate(dateForInternal.getDate());
+  var formattedDate = Utilities.formatDate(dateForInternal, Session.getScriptTimeZone(), "EEEE, MMMM dd, yyyy");
+  documentBody.replaceText(placeholderText, formattedDate);
+}
+
+// Helper Function: Create a copy of a template document with the desired title and destination folder
+function createDocumentCopy(templateId, title, destinationFolderId) {
+  var newFile = DriveApp.getFileById(templateId).makeCopy(title, DriveApp.getFolderById(destinationFolderId));
+  return DocumentApp.openById(newFile.getId());
+}
+
+// Helper Function: Get the week number of a date
+function getWeekNumber(date) {
+  var onejan = new Date(date.getFullYear(), 0, 1);
+  return Math.ceil(((date - onejan) / 86400000 + onejan.getDay() + 1) / 7);
+}
+
+// Primary function to create the agenda
+function createBiweeklyAgenda() {
+  // Check if the current week number is even (bi-weekly)
+  var today = new Date();
+  var weekNumber = getWeekNumber(today);
+  if (weekNumber % 2 === 0) {
     
-    if (table) {
-      const numRows = table.getNumRows();
-  
-      for (let i = 0; i < numRows; i++) { // Start from 0 as there is no header row now
-        const row = table.getRow(i);
-        const actionContent = row.getCell(0).getText();
-        actionList.push({ action: actionContent });
-      }
-    }
-  
-    return actionList;
+    // Create a new agenda document
+    var newDocument = createDocumentCopy(AGENDA_TEMPLATE_ID, "Temporary Title", AGENDA_FOLDER_ID);
+    var newDocumentId = newDocument.getId();
+
+    // Get the meeting date for the following Wednesday
+    var nextWednesday = getNextWednesday();
+
+    // Customize the title of the agenda
+    var agendaTitle = "Agenda for SMD LLM " + formatMeetingDate(nextWednesday);
+    DriveApp.getFileById(newDocumentId).setName(agendaTitle);
+
+    // Replace placeholder with the calculated meeting date in "Day, Month Date, YYYY" format
+    var document = DocumentApp.openById(newDocumentId);
+    var body = document.getBody();
+    replaceWithFormattedDate(body, "{{Day, Month Date, YYYY}}", nextWednesday);
   }
-  
-  // Run the actionsFromParagraphs function
-  actionsFromParagraphs();
-  
-  // Run the actionsFromTable function
-  actionsFromTable();
-  
-  // function to run both actionsFromParagraphs and actionsFromTable functions for the "Populate Actions" button.
-  function runBothActionItems() {
-    // Run actionsFromParagraphs function
-    actionsFromParagraphs();
-  
-    // Run actionsFromTable function
-    actionsFromTable();
-  }
-  
-  
+}
+
+// Test function for creating an agenda during an even week
+function testEvenWeekAgendaCreation() {
+  // Set weekNumber to an even value (e.g., 32) for testing
+  var weekNumber = 32; // Set this to an even value to simulate an even week
+
+  // Override the getWeekNumber function to return the test week number
+  var originalGetWeekNumber = getWeekNumber;
+  getWeekNumber = function() {
+    return weekNumber;
+  };
+
+  // Call the createBiweeklyAgenda function to test agenda creation
+  createBiweeklyAgenda();
+
+  // Reset the getWeekNumber function to its original implementation
+  getWeekNumber = originalGetWeekNumber;
+}
